e8f31a754bfa7cf6be3c13a22aeab06bb4cce141
vijay <vijaygohel5454@gmail.com>
2022-03-24 17:07:37 +0530

    notes array and props

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Note } from "./components/Note";
+import notes from "./note";
 
 const App = ()=>{
     return(
         <div>
             <Header />
-            <Note />        
+            {
+                notes.map((note)=>
+                    <Note title={note.title} content={note.content}/>        
+                )
+            }
             <Footer />
         </div>
     )
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export const Note = ()=>{
+export const Note = ({title, content})=>{
     return(
         <div className="note">
-            <h1>title</h1>
-            <p>content</p>
+            <h1>{title}</h1>
+            <p>{content}</p>
         </div>
     )
 }
